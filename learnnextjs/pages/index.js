bb356507b2db1bc5d2d752a856d9b2e8f34d2d37
jpaquim <joaopaquim@gmail.com>
2018-02-22 14:59:51 +0000

    Added solutions to learnnextjs - Blog

--- learnnextjs/pages/index.js
+++ learnnextjs/pages/index.js
@@ -2,28 +2,45 @@ import fetch from 'isomorphic-unfetch';
 import Link from 'next/link';
 import Layout from '../components/Layout';
 
-const Index = props => (
-  <Layout>
-    <h1>Batman TV Shows</h1>
-    <ul>
-      {props.shows.map(({ show }) => (
-        <li key={show.id}>
-          <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
-            <a>{show.name}</a>
-          </Link>
-        </li>
-      ))}
-    </ul>
-  </Layout>
+const getPosts = () => [
+  { id: 'hello-nextjs', title: 'Hello Next.js' },
+  { id: 'learn-nextjs', title: 'Learn Next.js is awesome' },
+  { id: 'deploy-nextjs', title: 'Deploy apps with ZEIT' },
+];
+
+const PostLink = ({ post }) => (
+  <li>
+    <Link as={`/p/${post.id}`} href={`/post?title=${post.title}`}>
+      <a>{post.title}</a>
+    </Link>
+    <style jsx>{`
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+      a {
+        text-decoration: none;
+        color: blue;
+      }
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
+  </li>
 );
 
-Index.getInitialProps = async () => {
-  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
-  const shows = await res.json();
-  console.log(`Show data fetched. Count: ${shows.length}`);
-  return {
-    shows,
-  };
-};
-
-export default Index;
+export default () => (
+  <Layout>
+    <h1>My Blog</h1>
+    <ul>{getPosts().map(post => <PostLink key={post.id} post={post} />)}</ul>
+    <style jsx>{`
+      h1,
+      a {
+        font-family: 'Arial';
+      }
+      ul {
+        padding: 0;
+      }
+    `}</style>
+  </Layout>
+);
